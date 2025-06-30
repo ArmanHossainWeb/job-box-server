323bc7cc26bc546c13bc96d065babe553731cfb3
ArmanHossain04 <armanhossain0477@gmail.com>
2025-06-30 17:58:06 +0600

    create jwt token and use axios to get the token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const cors = require("cors")
+const express = require('express');
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -9,8 +10,6 @@ require('dotenv').config()
 // midelWare 
 app.use(cors());
 app.use(express.json());
-
-
 
 
 
@@ -28,14 +27,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server	(optional starting in v4
+    // .7)
     await client.connect();
     
     const jobsCollection = client.db("jobBox").collection("jobs")
     const applicationsCollection = client.db("jobBox").collection("applications")
 
+    // jwt token releted api 
+    app.post("/jwt", async (req, res) => {
+      const {email} = req.body;
+      const user = {email}
+      const token = jwt.sign(user, "secret", {expiresIn: "1h"})
+      res.send({token})
+    })
+
+    
+
+
     // jobs api 
     app.get("/jobs", async (req,res)=>{
+      const email = req.query.email;
+      const query = {}
+      if (email) {
+        query.hr_email = email;
+      }
         const cursor = jobsCollection.find();
         const result = await cursor.toArray();
         res.send(result)
@@ -46,6 +62,15 @@ async function run() {
       const result = await jobsCollection.findOne(query)
       res.send(result)
     })
+    app.post("/jobs" , async (req, res)=>{
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob)
+      res.send(result)
+    })
+    
+
+
+
 
     // job  applications releted  api
     app.get("/applications", async (req,res)=> {
@@ -53,6 +78,8 @@ async function run() {
       const query = {
         applicant: email
       }
+
+      
       const result = await applicationsCollection.find(query).toArray()
       // bad way to aggregate data 
       for(const application of result){
@@ -67,12 +94,33 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/applications/job/:job_id", async (req, res)=>{
+      const job_id = req.params.job_id;
+      const query = {jobId : job_id}
+      const result = await applicationsCollection.find(query).toArray()
+      res.send(result)
+    } )
+
+
     app.post("/applications", async (req, res)=> {
       const applications = req.body;
       console.log(applications);
       const result = await applicationsCollection.insertOne(applications)
       res.send(result)
     }) 
+    app.patch("/applications/:id", async (req, res)=> {
+      const id = req.params.id;
+      const updated = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc ={
+        $set:{
+          status: req.body.status
+        }
+      }
+      const result = await applicationsCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+
+    })
 
 
     // Send a ping to confirm a successful connection
